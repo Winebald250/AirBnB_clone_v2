9d197e7573ba69f516ae0ba3e6aa93e519e380c5
Winebald250 <winebaldbanituze@gmail.com>
2024-04-15 12:05:47 -0700

    0x02. AirBnB clone - MySQL

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""Class Amenity inheriting from BaseModel"""
-from models.base_model import BaseModel
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    """Class representing Amenity"""
-
-    name = ""
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
